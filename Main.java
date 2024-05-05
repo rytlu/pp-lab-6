5ef8d6ac5537f90a11586b90bae7b4caa02f0d76
rytlu <kamil.rytel.kuc96@gmail.com>
2024-05-05 11:58:29 +0200

    chore: zad3

---
+++ Main.java
@@ -0,0 +1,24 @@
+import company.models.Manager;
+import company.models.Worker;
+
+
+public class Main {
+public static void main(String[] args) {
+        Worker worker1 = new Worker("Wojtek", 3500.0, 1, "01.01.2005", "Senior");
+        Worker worker2 = new Worker("Wiesiek", 2500.0, 2, "10.05.2023", "Junior");
+        Worker worker3 = new Worker("Zbyszek", 2100.0, 1, "11.10.2000", "Developer");
+
+        Manager manager = new Manager("Janusz", 10000.0, 3, "12.12.1945", "Menago");
+
+        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
+        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
+        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
+        System.out.println(manager.getName() + " has code: " + manager.hashCode());
+
+        System.out.println(worker3.getName() + " equals worker1: " + worker3.equals(worker1));
+        System.out.println(worker3.getName() + " equals worker2: " + worker3.equals(worker2));
+        System.out.println(worker3.getName() + " equals worker3: " + worker3.equals(worker3));
+        System.out.println(worker3.getName() + " equals manager: " + worker3.equals(manager));
+ 
+        }
+}
---
+++ company/abstracts/Employee.java
@@ -0,0 +1,50 @@
+package company.abstracts;
+
+import company.interfaces.Employable;
+
+public abstract class Employee implements Employable {
+    private String name;
+    private double salary;
+    private int id;
+    private String hireDate;
+    private String position;
+
+    public Employee(String name, double salary, int id, String hireDate, String position) {
+        this.name = name;
+        this.salary = salary;
+        this.id = id;
+        this.hireDate = hireDate;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof Employee)) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        Employee other = (Employee) obj;
+        return this.id == other.id;
+    }
+}
